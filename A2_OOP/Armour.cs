94b31be7919b043cbcf9167c887d8da205511b8b
Amy Wang <58236743+amywang480@users.noreply.github.com>
2020-09-15 23:26:36 -0400

    Update Armour.cs

--- A2_OOP/Armour.cs
+++ A2_OOP/Armour.cs
@@ -1,8 +1,5 @@
-﻿//Author:           Amy Wang
 //File Name:        Armour.cs
-//Project Name:     A2_OOP
 //Creation Date:    October 18, 2018
-//Modified Date:    October 22, 2018
 /*Description:      Create armour which includes helment and body armour*/
 
 using System;
