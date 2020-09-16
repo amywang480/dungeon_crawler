8663dd9d40e816c6a31469d24377d164888bf92e
Amy Wang <58236743+amywang480@users.noreply.github.com>
2020-09-15 23:31:41 -0400

    Update Room.cs

--- A2_OOP/Room.cs
+++ A2_OOP/Room.cs
@@ -1,8 +1,5 @@
-﻿//Author:           Amy Wang
 //File Name:        Room.cs
-//Project Name:     A2_OOP
 //Creation Date:    October 11, 2018
-//Modified Date:    October 22, 2018
 /*Description:      Store rooms of dungeon. Room class itself is used for passage rooms*/
 
 using System;
