990b1920b2a221c0170d70199799816e35a477c1
Amy Wang <58236743+amywang480@users.noreply.github.com>
2020-09-15 23:27:43 -0400

    Update Dungeon.cs

--- A2_OOP/Dungeon.cs
+++ A2_OOP/Dungeon.cs
@@ -1,8 +1,5 @@
-﻿//Author:           Amy Wang
 //File Name:        Dungeon.cs
-//Project Name:     A2_OOP
 //Creation Date:    October 11, 2018
-//Modified Date:    October 22, 2018
 /*Description:      Control all parts of the dungeon which consists of the main game elements, 
                     including the player and rooms*/
 
