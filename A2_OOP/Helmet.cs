fafe3f9709b3dbe4ee63b35ac1fea16e9d0022b4
Amy Wang <58236743+amywang480@users.noreply.github.com>
2020-09-15 23:28:37 -0400

    Update Helmet.cs

--- A2_OOP/Helmet.cs
+++ A2_OOP/Helmet.cs
@@ -1,8 +1,5 @@
-﻿//Author:           Amy Wang
 //File Name:        Helmet.cs
-//Project Name:     A2_OOP
 //Creation Date:    October 18, 2018
-//Modified Date:    October 22, 2018
 /*Description:      Create helmets which are buckets*/
 
 using System;
