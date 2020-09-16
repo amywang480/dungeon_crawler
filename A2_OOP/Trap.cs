51011f9958e9ff904c5cac2dbd989d805f5a435f
Amy Wang <58236743+amywang480@users.noreply.github.com>
2020-09-15 23:32:33 -0400

    Update Trap.cs

--- A2_OOP/Trap.cs
+++ A2_OOP/Trap.cs
@@ -1,8 +1,5 @@
-﻿//Author:           Amy Wang
 //File Name:        Trap.cs
-//Project Name:     A2_OOP
 //Creation Date:    October 11, 2018
-//Modified Date:    October 22, 2018
 /*Description:      Store traps that damage the player by reducing their health. Traps include active
                     and passive traps*/
 
