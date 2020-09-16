c4ff3edc27dcd08f6560480517f7687aa7461e62
Amy Wang <58236743+amywang480@users.noreply.github.com>
2020-09-15 23:27:21 -0400

    Update Body.cs

--- A2_OOP/Body.cs
+++ A2_OOP/Body.cs
@@ -1,9 +1,6 @@
-﻿//Author:           Amy Wang
 //File Name:        Body.cs
-//Project Name:     A2_OOP
 //Creation Date:    October 18, 2018
-//Modified Date:    October 22, 2018
-/*Description:      Create body armour which is a cookie sheet*/
+//Description:      Create body armour
 
 using System;
 using System.Collections.Generic;
