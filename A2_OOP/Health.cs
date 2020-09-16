7638ed1d36fd7c0be2b421747afb37b22f2a967e
Amy Wang <58236743+amywang480@users.noreply.github.com>
2020-09-15 23:28:17 -0400

    Update Health.cs

--- A2_OOP/Health.cs
+++ A2_OOP/Health.cs
@@ -1,8 +1,5 @@
-﻿//Author:           Amy Wang
 //File Name:        Health.cs
-//Project Name:     A2_OOP
 //Creation Date:    October 11, 2018
-//Modified Date:    October 22, 2018
 /*Description:      Create health item*/
 
 using System;
