809358dc56dc1e8f2df5da8d6d83a94b414b85bf
Amy Wang <58236743+amywang480@users.noreply.github.com>
2020-09-15 23:29:01 -0400

    Update Item.cs

--- A2_OOP/Item.cs
+++ A2_OOP/Item.cs
@@ -1,8 +1,5 @@
-﻿//Author:           Amy Wang
 //File Name:        Item.cs
-//Project Name:     A2_OOP
 //Creation Date:    October 11, 2018
-//Modified Date:    October 22, 2018
 /*Description:      Create items which includes health and time*/
 
 using System;
